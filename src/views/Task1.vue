5f23d1bd00f4f5b1f9dc97c1b2cffa105ef572d9
Rillian Diello <rilliansf@gmail.com>
2020-07-12 18:13:42 -0400

    Ajust

--- src/views/Task1.vue
+++ src/views/Task1.vue
@@ -4,9 +4,18 @@
     <pannel title="TASK 1 - Name">
      
      <div>
-       Create a database structure to store employee information
+       01110000 01110010 01101001 01101110 01110100 00100000 01101111 01110101
+        01110100 00100000 01111001 01101111 01110101 01110010 00100000 01101110
+        01100001 01101101 01100101 00100000 01110111 01101001 01110100 01101000
+        00100000 01101111 01101110 01100101 00100000 01101111 01100110 00100000
+        01110000 01101000 01110000 00100000 01101100 01101111 01101111 01110000
+        01110011
      </div>
-     
+     <h2> Translation</h2>
+     <div>
+       Print out your name with one of php loops:<br>
+       This answer is  in a file MyName.php
+     </div>
       
     </pannel>
   </page>
