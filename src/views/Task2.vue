733ab468c05325f16c3ac7948faf5a26111721ef
Rillian Diello <rilliansf@gmail.com>
2020-07-13 12:56:03 -0400

    REmoving debugger

--- src/views/Task2.vue
+++ src/views/Task2.vue
@@ -150,10 +150,8 @@ export default {
             if(data.errors.length == 0){
               this.tableTrue = true;
             }
-            debugger;
-            if(data.instalments.length > 6) {
-              console.log('fdsafdsa');
-            }
+            
+           
             this.itens = data;
           });
         console.log(response);
